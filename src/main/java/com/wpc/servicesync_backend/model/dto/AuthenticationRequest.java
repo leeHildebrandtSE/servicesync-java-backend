57cb08624f7753f69a7f76949c568a4547a71839
leeHildebrandtSE <leehildebrandt@rockmission.co.za>
2025-08-15 19:12:01 +0200

    Fixed compile issue

--- src/main/java/com/wpc/servicesync_backend/model/dto/AuthenticationRequest.java
+++ src/main/java/com/wpc/servicesync_backend/model/dto/AuthenticationRequest.java
@@ -3,9 +3,15 @@ package com.wpc.servicesync_backend.model.dto;
 import com.wpc.servicesync_backend.validation.ValidEmployeeId;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthenticationRequest {
     @NotBlank(message = "Employee ID is required")
     @ValidEmployeeId
